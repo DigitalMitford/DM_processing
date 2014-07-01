0a904028cb475e844823fe10aa9177bd863cc4c6
ebeshero <ebbondar@gmail.com>
2014-06-30 20:57:22 -0400

    repaired JavaScript in web folder; more JavaScript to write
    still need to work on show/hide of 734+ divs in the right-most area of
    the Site Index webpage.

--- web/mitfordSI.js
+++ web/mitfordSI.js
@@ -8,127 +8,127 @@ function init() {
 
 function toggle() {
 var id = this.id;
-var hist_persons = document.getElementById("hist_persons");
-var histOrgs = document.getElementbyId("histOrgs");
-var histPlaces = document.getElementbyId("histPlaces");
-var events = document.getElementbyId("events");
-var plants = document.getElementbyId("plants");
+var hist_persons = document.getElementsByClassName("hist_persons");
+var histOrgs = document.getElementsByClassName("histOrgs");
+var histPlaces = document.getElementsByClassName("histPlaces");
+var events = document.getElementsByClassName("events");
+var plants = document.getElementsByClassName("plants");
 
-var fict_chars = document.getElementById("fict_chars");
-var fictOrgs = document.getElementbyId("fictOrgs");
-var archetypes = document.getElementbyId("archetypes");
-var fictPlaces = document.getElementbyId("fictPlaces");
+var fict_chars = document.getElementsByClassName("fict_chars");
+var fictOrgs = document.getElementsByClassName("fictOrgs");
+var archetypes = document.getElementsByClassName("archetypes");
+var fictPlaces = document.getElementsByClassName("fictPlaces");
 
-var artWorks = document.getElementbyId("artWorks");
-var refWorks = document.getElementbyId("refWorks");
-var periodical = document.getElementbyId("periodical");
-var litWorks = document.getElementbyId("litWorks");
-var currSchol = document.getElementbyId("currSchol");
-var archives = document.getElementbyId("archives");
+var artWorks = document.getElementsByClassName("artWorks");
+var refWorks = document.getElementsByClassName("refWorks");
+var periodical = document.getElementsByClassName("periodical");
+var litWorks = document.getElementsByClassName("litWorks");
+var currSchol = document.getElementsByClassName("currSchol");
+var archives = document.getElementsByClassName("archives");
 
-var pastEditors = document.getElementbyId("pastEditors");
-var Mitford_Team = document.getElementbyId("Mitford_Team");
+var pastEditors = document.getElementsByClassName("pastEditors");
+var Mitford_Team = document.getElementsByClassName("Mitford_Team");
 
 switch (id) {
  case "hist_persons_toggle": {
-  var e;
-  for (e=0; e < hist_persons.length; e++)
-  {hist_persons[e].classList.toggle("on")}
-   };
+  var a;
+  for (a=0; a < hist_persons.length; a++)
+  {hist_persons[a].classList.toggle("on")}
+   }
 break;
   case "hist_orgs_toggle": {
-  var e;
-  for (e=0; e < histOrgs.length; e++)
-  {histOrgs[e].classList.toggle("on")}
-   };
+  var b;
+  for (b=0; b < histOrgs.length; b++)
+  {histOrgs[b].classList.toggle("on")}
+   }
   break;
   case "places_hist_toggle": {
-  var e;
-  for (e=0; e < histPlaces.length; e++)
-  {histPlaces[e].classList.toggle("on")}
-   };
+  var c;
+  for (c=0; c < histPlaces.length; c++)
+  {histPlaces[c].classList.toggle("on")}
+   }
   break;
   case "events_toggle": {
-  var e;
-  for (e=0; e < events.length; e++)
-  {events[e].classList.toggle("on")}
-   };
+  var d;
+  for (d=0; d < events.length; d++)
+  {events[d].classList.toggle("on")}
+   }
   break;
   case "plant_toggle": {
   var e;
   for (e=0; e < plants.length; e++)
   {plants[e].classList.toggle("on")}
-   };
+   }
   break;
   case "fict_chars_toggle": {
-  var e;
-  for (e=0; e < fict_chars.length; e++)
-  {fict_chars[e].classList.toggle("on")}
-   };
+  var f;
+  for (f=0; f < fict_chars.length; f++)
+  {fict_chars[f].classList.toggle("on")}
+   }
   break;
   case "fict_orgs_toggle": {
-  var e;
-  for (e=0; e < fictOrgs.length; e++)
-  {fictOrgs[e].classList.toggle("on")}
-   };
+  var g;
+  for (g=0; g < fictOrgs.length; g++)
+  {fictOrgs[g].classList.toggle("on")}
+   }
   break;
   case "archetypes_toggle": {
-  var e;
-  for (e=0; e < archetypes.length; e++)
-  {archetypes[e].classList.toggle("on")}
-   };
+  var h;
+  for (h=0; h < archetypes.length; h++)
+  {archetypes[h].classList.toggle("on")}
+   }
   break;
   case "places_fict_toggle": {
-  var e;
-  for (e=0; e < fictPlaces.length; e++)
-  {fictPlaces[e].classList.toggle("on")}
-   };
+  var i;
+  for (i=0; i < fictPlaces.length; i++)
+  {fictPlaces[i].classList.toggle("on")}
+   }
   break;
   case "art_toggle": {
-  var e;
-  for (e=0; e < artWorks.length; e++)
-  {artWorks[e].classList.toggle("on")}
-   };
+  var j;
+  for (j=0; j < artWorks.length; j++)
+  {artWorks[j].classList.toggle("on")}
+   }
   break;
   case "ref_19thc_toggle": {
-  var e;
-  for (e=0; e < refWorks.length; e++)
-  {refWorks[e].classList.toggle("on")}
-   };
+  var k;
+  for (k=0; k < refWorks.length; k++)
+  {refWorks[k].classList.toggle("on")}
+   }
   break;
   case "per_19thc_toggle": {
-  var e;
-  for (e=0; e < periodical.length; e++)
-  {periodical[e].classList.toggle("on")}
-   };
+  var l;
+  for (l=0; l < periodical.length; l++)
+  {periodical[l].classList.toggle("on")}
+   }
   break;
   case "literary": {
-  var e;
-  for (e=0; e < litWorks.length; e++)
-  {litWorks[e].classList.toggle("on")}
-   };
+  var m;
+  for (m=0; m < litWorks.length; m++)
+  {litWorks[m].classList.toggle("on")}
+   }
   break;
    case "currSchol_toggle": {
-  var e;
-  for (e=0; e < currSchol.length; e++)
-  {currSchol[e].classList.toggle("on")}
-   };
+  var n;
+  for (n=0; n < currSchol.length; n++)
+  {currSchol[n].classList.toggle("on")}
+   }
   break;
    case "archives_toggle": {
-  var e;
-  for (e=0; e < archives.length; e++)
-  {archives[e].classList.toggle("on")}
-   };
+  var o;
+  for (o=0; o < archives.length; o++)
+  {archives[o].classList.toggle("on")}
+   }
   break; case "Past_Editors_toggle": {
-  var e;
-  for (e=0; e < pastEditors.length; e++)
-  {pastEditors[e].classList.toggle("on")}
-   };
+  var p;
+  for (p=0; p < pastEditors.length; p++)
+  {pastEditors[p].classList.toggle("on")}
+   }
   break; case "Mitford_Team_toggle": {
-  var e;
-  for (e=0; e < Mitford_Team.length; e++)
-  {Mitford_Team[e].classList.toggle("on")}
-   };
+  var q;
+  for (q=0; q < Mitford_Team.length; q++)
+  {Mitford_Team[q].classList.toggle("on")}
+   }
   break;
   
 }
