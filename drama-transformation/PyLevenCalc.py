f1f4b736224bf137d9882e4db01bec465fbba878
ebeshero <ebbondar@gmail.com>
2016-10-16 16:37:26 -0400

    output all possible Levenshtein distances for Rienzi eds

---
+++ drama-transformation/PyLevenCalc.py
@@ -0,0 +1,18 @@
+import os
+from Levenshtein import distance
+os.chdir('/Users/ebb8/Documents/GitHub/mitford/drama-transformation')
+file = open("LevDistsRienzi.txt", "w")
+f = open('Rienzi_appText.txt')
+f.readline() # read and ignore the first line
+for line in f: # iterate over the remaining lines
+	v = line.split('\t')
+	dist1_2 = distance(v[1], v[2])
+	dist1_3 = distance(v[1], v[3])
+	dist1_4 = distance(v[1], v[4])
+	dist2_3 = distance(v[2], v[3])
+	dist2_4 = distance(v[2], v[4])
+	dist3_4 = distance(v[3], v[4])
+	file.write(''.join((v[0], '\t', 'MS::28:37:54: ', str(dist1_2), ', ', str(dist1_3), ', ', str(dist1_4), '\t', '28::37:54: ', str(dist2_3), ', ', str(dist2_4), '\t', '37::54: ', str(dist3_4), '\n')))
+f.close()
+file.close()		
+		
